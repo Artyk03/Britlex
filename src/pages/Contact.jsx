76a3e3644f36932f73d4f1ea57f280d66049b2ef
artyk03 <ikramowartyk03@gmail.com>
2024-04-07 05:11:39 -0700

    Atalar sozi Artygyn sozi

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import OutlinedButton from "../component/OutlinedButton";
+import ContainedButton from "../component/ContainedButton";
 import { Stack, Typography } from "@mui/material";
 import { titleName } from "../style/style.mjs";
 
@@ -9,6 +10,7 @@ const Contact = () => {
       <Typography style={titleName}>this is contact page</Typography>
       <Stack direction="row" mt={5} spacing={5} ml={5}>
         <OutlinedButton />
+        <ContainedButton />
       </Stack>
     </div>
   );
